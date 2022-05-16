1a06fe39b13ab85c47540625a1b05a98c282700e
Iago Ortiz <iago.ortiz-duran@iesruizgijon.com>
2022-05-16 10:31:15 +0200

    Creada clase calculadora con atributos y constructor

---
+++ src/calculadoragit/calculadora.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadoragit;
+
+/**
+ *
+ * @author iagom
+ */
+public class calculadora {
+    private int num1, num2;
+    private String marca, modelo;
+
+    public calculadora(int num1, int num2, String marca, String modelo) {
+        this.num1 = num1;
+        this.num2 = num2;
+        this.marca = marca;
+        this.modelo = modelo;
+    }
+    
+    
+}
