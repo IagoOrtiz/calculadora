1fdcff36c621058599b4e3d7c04b7abfaee93f79
Iago Ortiz <iago.ortiz-duran@iesruizgijon.com>
2022-05-16 10:49:09 +0200

    Añadidos getter y setter

--- src/calculadoragit/calculadora.java
+++ src/calculadoragit/calculadora.java
@@ -19,6 +19,39 @@ public class calculadora {
         this.marca = marca;
         this.modelo = modelo;
     }
+
+    public int getNum1() {
+        return num1;
+    }
+
+    public int getNum2() {
+        return num2;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setNum1(int num1) {
+        this.num1 = num1;
+    }
+
+    public void setNum2(int num2) {
+        this.num2 = num2;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+    
     
     
 }
